2faff24d3de92b0fc1fd23916ea7146576e6c3cf
Jakub Vohryzek <jakub@MacBook-Pro-3.local>
2026-05-24 15:09:30 +0200

    Add Barcelona atmosphere pass

--- src/main.js
+++ src/main.js
@@ -128,18 +128,86 @@ function createWorld() {
   addBox(base, [6.4, 0, 0], [1.7, 0.18, WORLD_DEPTH], 0x2b543c);
   addBox(base, [0, 0.03, 0], [WORLD_WIDTH, 0.16, WORLD_DEPTH], 0xdabf82);
   addPaving(base);
+  addMediterraneanFacades(base);
+  addGothicSideAlleys(base);
   addBoqueria(base);
+  addTerraces(base);
+  addFlowerStands(base);
   addStreetProps(base);
+  addBenches(base);
+  addAmbientCrowd(base);
+  addPolicePatrols(base);
+  addPigeons(base);
 
   for (let z = -2.65; z <= 3.05; z += 1.16) {
     addTree(base, -5.72, z + 0.12);
     addTree(base, 5.75, z - 0.28);
   }
 
+  addPalmTree(base, -4.92, -3.05);
+  addPalmTree(base, 4.98, 2.92);
+
   addBox(base, [0, 0.08, -4.03], [16, 0.24, 0.55], 0xb85845);
   addBox(base, [0, 0.21, -3.83], [16, 0.05, 0.12], 0xd1754f);
   addBox(base, [0, 0.08, 4.03], [16, 0.24, 0.55], 0xb85845);
   addBox(base, [0, 0.21, 3.83], [16, 0.05, 0.12], 0xd1754f);
+}
+
+function addMediterraneanFacades(parent) {
+  const facades = [
+    { x: -4.8, z: -4.52, w: 1.4, h: 1.35, color: 0xd99b6d, sign: "TAPAS" },
+    { x: -3.3, z: -4.52, w: 1.25, h: 1.6, color: 0xf0c789, sign: "CAFÈ" },
+    { x: -1.95, z: -4.52, w: 1.25, h: 1.28, color: 0xc76f55, sign: "HOSTAL" },
+    { x: 0.2, z: -4.52, w: 1.9, h: 1.7, color: 0xe3b37a, sign: "RAMBLA" },
+    { x: 2.35, z: -4.52, w: 1.45, h: 1.42, color: 0xd88a63, sign: "GELAT" },
+    { x: 4.05, z: -4.52, w: 1.55, h: 1.65, color: 0xf1cc8f, sign: "SANGRIA" },
+    { x: -4.2, z: 4.52, w: 1.75, h: 1.55, color: 0xe7bf83, sign: "BAR" },
+    { x: -1.95, z: 4.52, w: 1.65, h: 1.38, color: 0xc96d57, sign: "FLORS" },
+    { x: 0.4, z: 4.52, w: 1.75, h: 1.72, color: 0xf0d19b, sign: "PENSIÓ" },
+    { x: 2.65, z: 4.52, w: 1.55, h: 1.45, color: 0xdf9c6e, sign: "TAPES" },
+    { x: 4.55, z: 4.52, w: 1.3, h: 1.35, color: 0xd0a16f, sign: "SOL" },
+  ];
+
+  facades.forEach((facade) => {
+    addFacade(parent, facade);
+  });
+}
+
+function addFacade(parent, { x, z, w, h, color, sign }) {
+  const depth = 0.26;
+  const face = addBox(parent, [x, h / 2, z], [w, h, depth], color);
+  face.castShadow = false;
+
+  const trimColor = darken(color, 0.78);
+  addBox(parent, [x, h + 0.08, z], [w + 0.12, 0.16, depth + 0.05], trimColor);
+
+  const windowCount = Math.max(2, Math.floor(w / 0.42));
+  for (let i = 0; i < windowCount; i += 1) {
+    const wx = x - w / 2 + 0.27 + i * ((w - 0.54) / Math.max(windowCount - 1, 1));
+    addBox(parent, [wx, h * 0.62, z + Math.sign(z) * -0.15], [0.2, 0.36, 0.06], 0x2f4858);
+    addBox(parent, [wx, h * 0.38, z + Math.sign(z) * -0.15], [0.26, 0.08, 0.08], 0x4b2f2a);
+  }
+
+  const label = makeLabel(sign, "#fff3d6", "#4f321f", 192, 64);
+  label.position.set(x, 0.42, z - Math.sign(z) * 0.17);
+  label.scale.set(0.006, 0.006, 0.006);
+  parent.add(label);
+}
+
+function addGothicSideAlleys(parent) {
+  const alleys = [
+    [-6.08, -2.85],
+    [-6.08, 1.95],
+    [6.08, -1.35],
+    [6.08, 2.5],
+  ];
+
+  alleys.forEach(([x, z]) => {
+    addBox(parent, [x, 0.08, z], [1.35, 0.05, 0.62], 0x27313b);
+    addBox(parent, [x, 0.55, z - 0.27], [1.25, 0.9, 0.12], 0x44515a);
+    addBox(parent, [x, 0.55, z + 0.27], [1.25, 0.9, 0.12], 0x35434e);
+    addBox(parent, [x, 1.05, z], [1.15, 0.18, 0.62], 0x2c3842);
+  });
 }
 
 function addPaving(parent) {
@@ -195,6 +263,61 @@ function addBoqueria(parent) {
   addFruit(parent, -5.65, 0.58, -0.25, 0xa3c85a);
 }
 
+function addTerraces(parent) {
+  const terraces = [
+    { x: -4.35, z: -2.15, color: 0xffd166 },
+    { x: -4.15, z: 1.55, color: 0xf4a261 },
+    { x: 4.55, z: -2.15, color: 0xf7d488 },
+    { x: 4.25, z: 1.85, color: 0xffc857 },
+  ];
+
+  terraces.forEach((terrace) => {
+    addTerraceSet(parent, terrace.x, terrace.z, terrace.color);
+  });
+}
+
+function addTerraceSet(parent, x, z, umbrellaColor) {
+  addCylinder(parent, [x, 0.32, z], 0.34, 0.34, 0.08, 0x7a4e31);
+  addCylinder(parent, [x, 0.18, z], 0.035, 0.045, 0.32, 0x4f321f);
+  addCylinder(parent, [x, 0.72, z], 0.05, 0.05, 0.9, 0x6b4a2f);
+  addCone(parent, [x, 1.25, z], 0.78, 0.34, umbrellaColor);
+
+  const chairOffsets = [
+    [-0.56, -0.1],
+    [0.56, 0.1],
+    [-0.1, 0.56],
+  ];
+  chairOffsets.forEach(([ox, oz]) => {
+    addBox(parent, [x + ox, 0.24, z + oz], [0.24, 0.28, 0.24], 0x2f4858);
+    addBox(parent, [x + ox, 0.47, z + oz + 0.05], [0.24, 0.3, 0.06], 0x365f75);
+  });
+
+  addSangriaGlass(parent, x - 0.12, z + 0.06);
+  addSangriaGlass(parent, x + 0.13, z - 0.08);
+}
+
+function addSangriaGlass(parent, x, z) {
+  addCylinder(parent, [x, 0.45, z], 0.045, 0.035, 0.22, 0xe76f51);
+  addCylinder(parent, [x, 0.59, z], 0.05, 0.05, 0.025, 0xf7f0d0);
+}
+
+function addFlowerStands(parent) {
+  [
+    [-5.05, 0.72],
+    [5.12, -0.68],
+  ].forEach(([x, z]) => {
+    addBox(parent, [x, 0.25, z], [0.72, 0.34, 0.42], 0x5b3327);
+    addBox(parent, [x, 0.5, z], [0.78, 0.12, 0.48], 0x7a4e31);
+
+    const flowerColors = [0xe63946, 0xffafcc, 0xffd166, 0x8ecae6, 0xb7e4c7];
+    flowerColors.forEach((color, index) => {
+      const fx = x - 0.26 + index * 0.13;
+      addCylinder(parent, [fx, 0.72, z], 0.045, 0.025, 0.18, 0x2f7a48);
+      addFruit(parent, fx, 0.85, z + (index % 2 === 0 ? 0.12 : -0.08), color);
+    });
+  });
+}
+
 function addStreetProps(parent) {
   addBox(parent, [4.0, 0.34, -2.45], [1.1, 0.16, 0.38], 0x7a4e31);
   addBox(parent, [3.55, 0.15, -2.32], [0.11, 0.38, 0.11], 0x4f321f);
@@ -205,6 +328,24 @@ function addStreetProps(parent) {
 
   addBox(parent, [6.0, 0.32, 0.5], [0.38, 0.64, 0.38], 0x36454f);
   addBox(parent, [6.0, 0.68, 0.31], [0.3, 0.1, 0.04], 0x96d2e0);
+}
+
+function addBenches(parent) {
+  [
+    [-2.4, -3.0, 0],
+    [1.25, -3.0, 0],
+    [-0.95, 3.02, Math.PI],
+    [3.1, 3.02, Math.PI],
+  ].forEach(([x, z, rotation]) => {
+    const bench = new THREE.Group();
+    addBox(bench, [0, 0.28, 0], [0.86, 0.12, 0.24], 0x7a4e31);
+    addBox(bench, [0, 0.47, 0.12], [0.86, 0.28, 0.09], 0x8a5a3b);
+    addBox(bench, [-0.32, 0.13, 0], [0.08, 0.24, 0.08], 0x4f321f);
+    addBox(bench, [0.32, 0.13, 0], [0.08, 0.24, 0.08], 0x4f321f);
+    bench.position.set(x, 0, z);
+    bench.rotation.y = rotation;
+    parent.add(bench);
+  });
 }
 
 function addTree(parent, x, z) {
@@ -231,6 +372,89 @@ function addTree(parent, x, z) {
     crown.castShadow = true;
     crown.receiveShadow = true;
     parent.add(crown);
+  });
+}
+
+function addPalmTree(parent, x, z) {
+  const trunk = addCylinder(parent, [x, 0.7, z], 0.09, 0.13, 1.4, 0x9b6a3c);
+  trunk.rotation.z = 0.12;
+
+  const leafMaterial = new THREE.MeshStandardMaterial({
+    color: 0x2f8f5b,
+    roughness: 0.86,
+    flatShading: true,
+  });
+
+  for (let i = 0; i < 7; i += 1) {
+    const leaf = new THREE.Mesh(new THREE.ConeGeometry(0.16, 1.05, 4), leafMaterial.clone());
+    leaf.position.set(x, 1.45, z);
+    leaf.rotation.z = Math.PI / 2;
+    leaf.rotation.y = (i / 7) * Math.PI * 2;
+    leaf.castShadow = true;
+    parent.add(leaf);
+  }
+}
+
+function addAmbientCrowd(parent) {
+  const people = [
+    [-5.15, -2.3, 0xffd166, 0x486073],
+    [-4.85, 2.35, 0x8ecae6, 0x34495e],
+    [-3.85, -3.08, 0xf2b5d4, 0x34495e],
+    [3.82, 2.95, 0xffafcc, 0x465166],
+    [4.95, -2.75, 0xa7c957, 0x3e4c59],
+    [5.2, 1.7, 0xf4a261, 0x3e4c59],
+  ];
+
+  people.forEach(([x, z, shirt, shorts], index) => {
+    const person = createPerson({
+      shirt,
+      shorts,
+      skin: index === 4 ? 0xff6f5e : 0xffd6a5,
+      hair: index % 2 === 0 ? 0x6b4a2f : 0x8a5a3b,
+      scale: 0.66,
+    });
+    person.position.set(x, 0.02, z);
+    person.rotation.y = index % 2 === 0 ? -0.4 : 0.7;
+    parent.add(person);
+  });
+}
+
+function addPolicePatrols(parent) {
+  [
+    [-4.15, -3.22, 0.2],
+    [4.68, 3.05, -0.5],
+  ].forEach(([x, z, rotation]) => {
+    const officer = createPerson({
+      shirt: 0x234b6d,
+      shorts: 0x1c2e42,
+      skin: 0xffd6a5,
+      hair: 0x1c2e42,
+      scale: 0.68,
+    });
+    officer.position.set(x, 0.02, z);
+    officer.rotation.y = rotation;
+    addBox(officer, [0, 1.36, -0.02], [0.34, 0.08, 0.22], 0x1c2e42);
+    addBox(officer, [0.28, 0.78, 0], [0.09, 0.3, 0.06], 0xf1c40f);
+    parent.add(officer);
+  });
+}
+
+function addPigeons(parent) {
+  [
+    [-1.9, 1.15],
+    [-1.55, 1.34],
+    [2.95, -0.95],
+    [3.25, -0.76],
+    [0.65, -2.85],
+  ].forEach(([x, z], index) => {
+    const pigeon = new THREE.Group();
+    addCylinder(pigeon, [0, 0.11, 0], 0.06, 0.09, 0.18, 0x87919a);
+    addFruit(pigeon, 0.08, 0.18, -0.03, 0x5f6972);
+    addCone(pigeon, [0.16, 0.18, -0.04], 0.035, 0.08, 0xd99b6d);
+    addBox(pigeon, [-0.08, 0.1, 0.05], [0.14, 0.025, 0.08], 0x5f6972);
+    pigeon.position.set(x, 0.03, z);
+    pigeon.rotation.y = index * 0.85;
+    parent.add(pigeon);
   });
 }
 
@@ -635,6 +859,21 @@ function addCylinder(parent, position, topRadius, bottomRadius, height, color) {
     new THREE.CylinderGeometry(topRadius, bottomRadius, height, 6),
     material,
   );
+  mesh.position.set(position[0], position[1], position[2]);
+  mesh.castShadow = true;
+  mesh.receiveShadow = true;
+  parent.add(mesh);
+  return mesh;
+}
+
+function addCone(parent, position, radius, height, color) {
+  const material = new THREE.MeshStandardMaterial({
+    color,
+    roughness: 0.86,
+    metalness: 0,
+    flatShading: true,
+  });
+  const mesh = new THREE.Mesh(new THREE.ConeGeometry(radius, height, 7), material);
   mesh.position.set(position[0], position[1], position[2]);
   mesh.castShadow = true;
   mesh.receiveShadow = true;
